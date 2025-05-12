ae827ff17e7a183c9285c989cef2f23f4d144233
wj-jason <jason.wang1709@gmail.com>
2025-05-12 19:37:05 -0400

    a

---
+++ sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+#define dbg(v) cout << "Line " << __LINE__ << " -> " << #v << " = " << (v) << '\n';
+#define here() cout << "At Line "<<  __LINE__ << '\n';
+
+void solve() {
+}
+
+
+int main() {
+    // string name = "stacking";
+    // freopen((name+".in").c_str(), "r", stdin);
+    // freopen((name+".out").c_str(), "w", stdout);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+    return 0;
+}
